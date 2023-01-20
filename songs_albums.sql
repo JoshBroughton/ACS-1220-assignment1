8ca00cffca126e26997fcfc7bb0833bb7d8f3d00
Josh Broughton <josh.broughton47@gmail.com>
2023-01-20 09:58:08 -0700

    assignment complete

--- songs_albums.sql
+++ songs_albums.sql
@@ -7,11 +7,19 @@
  * Note that album - song is a one-to-many relationship, so no bridge table is needed.
  */
 
+
 CREATE TABLE Albums (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(70) NOT NULL,
     artist VARCHAR(70) NOT NULL,
     year_published INTEGER NOT NULL
+);
+
+CREATE TABLE Songs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(70) NOT NULL,
+    album_id INTEGER,
+    FOREIGN KEY(album_id) REFERENCES Albums(id)
 );
 
 /* 
@@ -28,6 +36,16 @@ VALUES
     ('California', 'Blink-182', 2016)
 ;
 
+INSERT INTO Songs
+    (name, album_id)
+VALUES
+    ('Carry That Weight', 2),
+    ("I'm On Fire", 4),
+    ('Time', 1),
+    ('Life in the Fast Lane', 3),
+    ('Brohemian Rhapsody', 5)
+;
+
 /* Required to get result in column format */
 .headers on
 .mode column
@@ -35,23 +53,33 @@ VALUES
 
 /* Queries */
 
--- SELECT * FROM Songs;
-
 SELECT * FROM Albums;
-
+SELECT * FROM Songs;
 /* 
  * TODO: Write a table join query to construct a table of Song Name : Album Name
  */
 
+SELECT Songs.name, Albums.name
+FROM Songs
+INNER JOIN Albums ON Songs.album_id=Albums.id;
+
 /*
  * TODO: Find all albums published between 1970 and 1980.
  */
-
+SELECT name, year_published FROM Albums
+WHERE year_published between 1970 AND 1980;
 /*
  * TODO: Find all songs on albums published between 1970 and 1980. 
  *(Hint: Use a table join.)
  */
- 
+SELECT Songs.name, Albums.year_published
+FROM Songs
+INNER JOIN Albums ON Songs.album_id=Albums.id
+WHERE year_published between 1970 AND 1980;
 /*
  * TODO: Find all songs on albums with names containing 'California'.
  */
+SELECT Songs.name, Albums.name
+FROM Songs
+INNER JOIN Albums ON Songs.album_id=Albums.id
+WHERE Albums.name LIKE '%California%';
